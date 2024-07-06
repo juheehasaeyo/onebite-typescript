aa4e74f389f36e83187822ea1711ad2eaf8e786b
조주희 <carla_cho@naver.com>
2024-07-06 21:15:29 +0900

    객체

---
+++ section02/src/chapter3.ts
@@ -0,0 +1,14 @@
+// object: 구조를 기준으로 타입 정의=> 구조적 타입 시스템=> property based type system
+let user: {
+    id?: number; // ? : 있어도 되고 없어도 된다 => 선택적 프로퍼티
+    name: string;
+} = {
+    id: 1,
+    name: "이세숑",
+};
+
+let config: {
+    readonly apiKey: string; // 수정 불가, 읽기 전용
+} = {
+    apiKey: "MY API KEY",
+};
